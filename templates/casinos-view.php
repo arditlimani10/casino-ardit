26964466133153ae256c3df31b9873988417925f
arditlimani10 <103512376+arditlimani10@users.noreply.github.com>
2022-04-12 19:15:29 +0200

    Created main nav of casino view and added some style

---
+++ templates/casinos-view.php
@@ -0,0 +1,12 @@
+<!-- Container of all elements -->
+<div class="container">
+    <!-- Main navigation container -->
+    <div class="casino-nav">
+        <ul>
+            <li class="casino">Casino</li>
+            <li class="bonus">Bonus</li>
+            <li class="features">Features</li>
+            <li class="play">Play</li>
+        </ul>
+    </div>
+</div>
